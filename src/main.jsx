f1ab74882f4950c6cc477ae2fe8872800bcef8ae
dzarrr <dzarbelahanifa@gmail.com>
2025-03-08 23:50:53 +0700

    feat: add sentry intregation

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
-import { message } from "antd";
+import * as Sentry from "@sentry/react";
 import "./index.css";
+
+Sentry.init({
+  dsn: "https://4444241bc4902ac74519801ea12a41d1@o4508943129051136.ingest.de.sentry.io/4508943133179984",
+  integrations: [Sentry.browserTracingIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+});
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
